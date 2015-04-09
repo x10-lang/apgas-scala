bdb718f3218577b0175bbd7a99e8c1d04a4764b5
Josh Milthorpe <jjmiltho@us.ibm.com>
2015-04-09 14:00:11 -0400

    Resilient KMeans

--- apgas.scala.examples/src/apgas/scala/examples/KMeans.scala
+++ apgas.scala.examples/src/apgas/scala/examples/KMeans.scala
@@ -17,6 +17,7 @@ import java.util.Random
 object KMeans {
   val DIM = 2
   val CLUSTERS = 4
+  val NUM_PLACES = 5
 
   class ClusterState extends Serializable {
     val clusters = Array.ofDim[Float](CLUSTERS, DIM)
@@ -24,24 +25,32 @@ object KMeans {
   }
 
   def main(args: Array[String]): Unit = {
-    val numPoints = 2000
-    val iterations = 50
-    
+    val numPoints: Int = try {
+      args(0).toInt
+    } catch {
+      case _: Throwable => 2000
+    }
+    val iterations: Int = try {
+      args(0).toInt
+    } catch {
+      case _: Throwable => 20
+    }
+
     // PS: I need this because java is not the default on my system..
     System.setProperty(Configuration.APGAS_JAVA, "java8")
 
     if (System.getProperty(Configuration.APGAS_PLACES) == null) {
-      System.setProperty(Configuration.APGAS_PLACES, "2")
+      System.setProperty(Configuration.APGAS_PLACES, NUM_PLACES.toString())
     }
-     
+
     val globalClusterState = PlaceLocalRef.forPlaces[ClusterState](places) {
       new ClusterState()
     }
-           
+
     val globalCurrentClusters = PlaceLocalRef.forPlaces[Array[Array[Float]]](places) {
       Array.ofDim[Float](CLUSTERS, DIM)
     }
-      
+
     val globalPoints = PlaceLocalRef.forPlaces(places) {
       val rand = new Random(here.id)
       Array.fill[Float](numPoints / places.size, DIM) {
@@ -50,8 +59,8 @@ object KMeans {
     }
 
     val centralCurrentClusters = Array.ofDim[Float](CLUSTERS, DIM)
-    val centralNewClusters     = Array.ofDim[Float](CLUSTERS, DIM)
-    val centralClusterCounts   = Array.ofDim[Int](CLUSTERS)
+    val centralNewClusters = Array.ofDim[Float](CLUSTERS, DIM)
+    val centralClusterCounts = Array.ofDim[Int](CLUSTERS)
 
     // arbitrarily initialize central clusters to first few points
     for (i <- 0 until CLUSTERS; j <- 0 until DIM) {
@@ -60,7 +69,7 @@ object KMeans {
 
     var iter = 1
     var converged = false
-    
+
     while (iter <= iterations && !converged) {
       println("Iteration: " + iter)
 
@@ -69,7 +78,7 @@ object KMeans {
           async {
             val placeClusters = at(place) {
               val currentClusters = globalCurrentClusters()
-              
+
               for (i <- 0 until CLUSTERS; j <- 0 until DIM) {
                 currentClusters(i)(j) = centralCurrentClusters(i)(j)
               }
@@ -120,7 +129,7 @@ object KMeans {
                   centralNewClusters(i)(j) += placeClusters.clusters(i)(j)
                 }
               }
-                        
+
               for (j <- 0 until CLUSTERS) {
                 centralClusterCounts(j) += placeClusters.clusterCounts(j)
               }
@@ -132,10 +141,10 @@ object KMeans {
       for (k <- 0 until CLUSTERS; d <- 0 until DIM) {
         centralNewClusters(k)(d) /= centralClusterCounts(k)
       }
-      
-      def testConvergence() : Boolean = {
+
+      def testConvergence(): Boolean = {
         import scala.math.abs
-        
+
         for (i <- 0 until CLUSTERS; j <- 0 until DIM) {
           if (abs(centralCurrentClusters(i)(j) - centralNewClusters(i)(j)) > 0.0001) {
             return false
@@ -143,7 +152,8 @@ object KMeans {
         }
         return true
       }
-      
+
+      iter += 1
       converged = testConvergence()
 
       for (i <- 0 until CLUSTERS; j <- 0 until DIM) {
@@ -153,12 +163,10 @@ object KMeans {
       for (i <- 0 until CLUSTERS; j <- 0 until DIM) {
         centralNewClusters(i)(j) = 0.0f
       }
-      
+
       for (i <- 0 until CLUSTERS) {
         centralClusterCounts(i) = 0
       }
-      
-      iter += 1
     }
 
     for (d <- 0 until DIM) {

66e8daf06abaa61d3cf89098803d716cea994e79
Philippe Suter <philippe.suter@gmail.com>
2015-05-15 14:45:48 -0400

    comments.

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,15 +1,22 @@
+# Make sure this is Java 8
 JAVA=java8
 JAVAC=javac8
 SCALAC=scalac
 
+# Where you keep you apgas SVN checkout.
 APGAS_COMMON_HOME=${HOME}/apgas
+
+# Where you keep you apgas-scala Git checkout.
 APGAS_SCALA_COMMON_HOME=${HOME}/workspace
 
+# Where you keep Scala. Should have a lib, bin, etc. subdir.
 SCALA_HOME=${HOME}/software/scala/current
+# Where you keep Akka, should have a lib/akka subdir.
 AKKA_HOME=${HOME}/software/akka/akka-2.3.9
 
-# You probably don't need to change what's below.
 
+
+# The rest depends only on the above.
 APGAS_HOME=${APGAS_COMMON_HOME}/apgas
 APGAS_EXAMPLES_HOME=${APGAS_COMMON_HOME}/apgas.examples
 APGAS_IMPL_HOME=${APGAS_COMMON_HOME}/apgas.impl
